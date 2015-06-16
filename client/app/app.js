54d1c49d0989fc34b0a453e0504312619c0303a6
ganne <ganne@digitaleo.com>
2015-06-16 08:30:05 +0200

    Nettoyage

--- client/app/app.js
+++ client/app/app.js
@@ -1,16 +1,22 @@
 'use strict';
+var user = new Parse.User();
+//user.set("username", "ganne");
+//user.set("password", "10jtal35");
+//user.set("email", "email@example.com");
 
+Parse.initialize("JyHA29BGTe9B12FlDdrmnTEgtc0etHocLU4uLyuq", "jx8aHTbzLnrsdQVL8T5ohllILd43bLWeWUQcbglp");
 angular.module('codingChallengeApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
 ])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .otherwise({
-        redirectTo: '/home'
-      });
+        .config(function ($routeProvider, $locationProvider)
+        {
+            $routeProvider
+                    .otherwise({
+                        redirectTo: '/home'
+                    });
 
-    $locationProvider.html5Mode(true);
-  });
+            $locationProvider.html5Mode(true);
+        });

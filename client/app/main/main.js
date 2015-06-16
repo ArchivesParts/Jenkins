0eb7057a43d10010898049396516dee11273067d
ganne <ganne@digitaleo.com>
2015-06-16 08:33:07 +0200

    migrate layout to adminlte 2

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -9,41 +9,4 @@ angular.module('codingChallengeApp')
                         controller: 'MainCtrl',
                         controllerAs: 'main'
                     });
-        }).provider('tools', function ()
-        {
-            var service = function ($http)
-            {
-
-                var isEditable = function (name)
-                {
-                    //2014-11-13 16:59:04
-                    var regExp = /\w+Id|^id/
-                    return !name.match(regExp);
-                };
-
-                return {
-                    isEditable: isEditable
-                };
-            }
-            this.$get = ['$http', service];
-        }).filter('unique', function ()
-        {
-            return function (arr, field)
-            {
-                return _.uniq(arr, function (a)
-                {
-                    return a[field];
-                });
-            };
-        }).filter('contain', function ()
-        {
-            return function (arr, field)
-            {
-                return _.filter(arr, function (a)
-                {
-                    var regexp = new RegExp(field);
-                    return (a.name && a.name.toLowerCase().match(regexp)) ||
-                           (a.description && a.description.toLowerCase().match(regexp));
-                });
-            };
         });
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,6 +1,42 @@
 'use strict';
 
 angular.module('codingChallengeApp')
-        .controller('NavbarCtrl', function ($scope, $location, $http)
+        .controller('NavbarCtrl', function ()
         {
-        });
+            var nav = this;
+            nav.menu = [
+                {
+                    name: 'Coding Game',
+                    icon: 'fa fa-gamepad',
+                    active: true,
+                    url: '/home'
+//                    counter: {
+//                        value: 4,
+//                        style: 'label-primary'
+//                    }
+                },
+                {
+                    name: 'Totems',
+                    icon: 'fa fa-dashboard',
+                    url: '/booked',
+                    child: [
+                        {
+                            name: 'Dashboard',
+                            url: '/booked',
+                            icon: 'fa fa-pie-chart'
+                        },
+                        {
+                            name: 'Booking',
+                            url: '/booking',
+                            icon: 'fa fa-bookmark'
+                        },
+                    ]
+                },
+                {
+                    name: 'Documentation',
+                    icon: 'fa fa-book'
+                },
+
+            ]
+        }
+);
